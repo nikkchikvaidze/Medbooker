51711284bec6e0490a6f342ed7a703a4b1bded8d
Nikolozi <nikkchikvaidze@gmail.com>
2024-06-13 23:19:55 +0400

    updated doctor search component & doctor dashboard component

--- src/app/pages/dashboard/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/pages/dashboard/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map, switchMap } from 'rxjs';
+import {
+  Booking,
+  BookingStatusUpdateRequest,
+  Status,
+  StatusChange,
+} from 'src/app/models';
+import { AuthService, BookingService } from 'src/app/services';
 
 @Component({
   selector: 'app-doctor-dashboard',
   templateUrl: './doctor-dashboard.component.html',
-  styleUrls: ['./doctor-dashboard.component.scss']
+  styleUrls: ['./doctor-dashboard.component.scss'],
 })
 export class DoctorDashboardComponent implements OnInit {
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService
+  ) {}
 
-  constructor() { }
+  bookings$: Observable<Booking[]> | undefined;
 
   ngOnInit(): void {
+    this.loadingBookings();
   }
 
+  loadingBookings() {
+    this.bookings$ = this.authService.getUser().pipe(
+      switchMap((user) => {
+        return this.bookingService
+          .getBookingForEntity(
+            user?.data.user?.user_metadata['entityNo'],
+            new Date().toISOString()
+          )
+          .pipe(
+            map((x) => x.bookingMap),
+            map((x) =>
+              Object.keys(x)
+                .map((key) => x[key])
+                .flat()
+            ),
+            map((x) =>
+              x.filter((x) => x.status === Status.TENTATIVE).splice(0, 5)
+            )
+          );
+      })
+    );
+  }
+
+  onStatusChange(status: StatusChange) {
+    const bookingUpdateBody: BookingStatusUpdateRequest = {
+      bookingStatus: status.status,
+      comment: '',
+      includeDependent: true,
+    };
+    this.bookingService
+      .updateBooking(status.id, bookingUpdateBody)
+      .subscribe(() => this.loadingBookings());
+  }
 }
--- src/app/pages/doctor-search/doctor-search.component.ts
+++ src/app/pages/doctor-search/doctor-search.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Observable, debounceTime, switchMap, tap } from 'rxjs';
+import { Doctor } from 'src/app/models';
+import { DoctorService } from 'src/app/services';
+import { capitalize } from 'src/app/shared/utils/capitalize';
 
 @Component({
   selector: 'app-doctor-search',
   templateUrl: './doctor-search.component.html',
-  styleUrls: ['./doctor-search.component.scss']
+  styleUrls: ['./doctor-search.component.scss'],
 })
 export class DoctorSearchComponent implements OnInit {
+  constructor(private doctorService: DoctorService, private fb: FormBuilder) {}
 
-  constructor() { }
+  searchInput = new FormControl();
+  doctors$: Observable<Doctor[]> | undefined;
 
   ngOnInit(): void {
+    this.loadAllDoctors();
+
+    this.searchInput.valueChanges.pipe(
+      debounceTime(1000),
+      switchMap((value) => {
+        const [firstName, lastName] = value.toLowerCase().trim().split(' ');
+        return this.doctorService.searchForDoctor(
+          capitalize(firstName),
+          capitalize(lastName)
+        );
+      })
+    );
   }
 
+  loadAllDoctors(): void {
+    this.doctors$ = this.doctorService.getAllDoctors();
+  }
 }
