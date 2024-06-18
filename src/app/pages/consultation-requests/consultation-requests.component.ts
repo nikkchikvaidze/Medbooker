692fc5984ba14f42a96ae2dda7dcd0a3f447ffc5
Nikolozi <nikkchikvaidze@gmail.com>
2024-06-19 03:11:34 +0400

    added consultation-requests

--- src/app/pages/consultation-requests/consultation-requests.component.ts
+++ src/app/pages/consultation-requests/consultation-requests.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import {
+  Booking,
+  BookingStatusUpdateRequest,
+  Status,
+  StatusChange,
+} from 'src/app/models';
+import { AuthService, BookingService } from 'src/app/services';
 
 @Component({
   selector: 'app-consultation-requests',
   templateUrl: './consultation-requests.component.html',
-  styleUrls: ['./consultation-requests.component.scss']
+  styleUrls: ['./consultation-requests.component.scss'],
 })
 export class ConsultationRequestsComponent implements OnInit {
+  bookings$: Observable<Booking[]> | undefined;
+  entityNo!: number;
 
-  constructor() { }
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.loadingBookings();
   }
 
+  loadingBookings(): void {
+    this.authService
+      .getUser()
+      .subscribe(
+        (user) => (this.entityNo = user?.data.user?.user_metadata['entityNo'])
+      );
+    this.bookings$ = this.bookingService
+      .getBookingForEntity(this.entityNo, new Date().toISOString())
+      .pipe(
+        map((x) => x.bookingMap),
+        map((x) =>
+          Object.keys(x)
+            .map((key) => x[key])
+            .flat()
+            .sort(
+              (a, b) =>
+                Number(new Date(a.startTime)) - Number(new Date(b.startTime))
+            )
+        ),
+        map((x) => x.filter((x) => x.status === Status.TENTATIVE))
+      );
+  }
+
+  onStatusChange(status: StatusChange) {
+    const bookingUpdateBody: BookingStatusUpdateRequest = {
+      bookingStatus: status.status,
+      comment: '',
+      includeDependent: true,
+    };
+    this.bookingService
+      .updateBooking(status.id, bookingUpdateBody)
+      .subscribe(() => this.loadingBookings());
+  }
 }
