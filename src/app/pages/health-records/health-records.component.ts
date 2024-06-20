bf341800689fb0844067757b5ed2f4ea65de1d3e
Nikolozi <nikkchikvaidze@gmail.com>
2024-06-20 22:26:54 +0400

    added health records & patients components

--- src/app/pages/health-records/health-records.component.ts
+++ src/app/pages/health-records/health-records.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map, of, switchMap } from 'rxjs';
+import { AttendeeType, Booking, Status, UpcomingBooking } from 'src/app/models';
+import { Roles } from 'src/app/models/user.model';
+import { AuthService, BookingService, DoctorService } from 'src/app/services';
+import { flattenBookings } from 'src/app/shared/utils/helpers.fn';
 
 @Component({
   selector: 'app-health-records',
   templateUrl: './health-records.component.html',
-  styleUrls: ['./health-records.component.scss']
+  styleUrls: ['./health-records.component.scss'],
 })
 export class HealthRecordsComponent implements OnInit {
+  passedBookings$: Observable<UpcomingBooking[]> | undefined;
+  selectedBooking: UpcomingBooking | undefined;
+  attendee!: AttendeeType;
+  role: number | undefined;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private bookingService: BookingService,
+    private doctorService: DoctorService
+  ) {}
 
   ngOnInit(): void {
+    this.authService
+      .getUser()
+      .subscribe((user) => (this.role = user?.['entityNo']));
+    this.loadPassedBookings();
   }
 
+  loadPassedBookings() {
+    if (!this.role) return;
+    this.passedBookings$ = this.bookingService
+      .getBookingForEntity(this.role, undefined, new Date().toISOString())
+      .pipe(
+        map((bookings) =>
+          flattenBookings(bookings)
+            .filter((element) => element.status == Status.CONFIRMED)
+            .sort(
+              (a, b) =>
+                Number(new Date(a.startTime)) - Number(new Date(b.startTime))
+            )
+        ),
+        switchMap((bookings) => {
+          if (this.role == Roles.Doctor) {
+            return of(bookings);
+          }
+          return this.doctorService.getAllDoctors().pipe(
+            map((doctors) => {
+              return bookings.map((booking) => {
+                const doctorsEntity = booking.attendees.find(
+                  (attendee) => attendee.attendeeType == AttendeeType.PROVIDER
+                )?.entityNo;
+                return {
+                  ...booking,
+                  practiceName: doctors.find(
+                    (doctor) => doctor.entityNo == doctorsEntity
+                  )?.practiceName,
+                };
+              });
+            })
+          );
+        })
+      );
+  }
+  getSingleBooking(booking: Booking) {
+    this.selectedBooking = booking;
+  }
 }
--- src/app/pages/patients/patients.component.ts
+++ src/app/pages/patients/patients.component.ts
@@ -1,15 +1,124 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { User, UserMetadata } from '@supabase/supabase-js';
+import { Observable, debounceTime } from 'rxjs';
+import {
+  AttendeeType,
+  BookingStatusUpdateRequest,
+  Patient,
+  Status,
+} from 'src/app/models';
+import { Roles } from 'src/app/models/user.model';
+import { AuthService, BookingService } from 'src/app/services';
+import { PatientService } from 'src/app/services/patient.service';
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
-  styleUrls: ['./patients.component.scss']
+  styleUrls: ['./patients.component.scss'],
 })
 export class PatientsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  patients$: Observable<Patient[]> | undefined;
+  searchForm: FormGroup | undefined;
+  selectedPatient: Patient | undefined;
+  showAdditionalInformation = false;
+  currentUser: UserMetadata | undefined;
+  pickedTime = '';
+  get today() {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    return date;
   }
 
+  constructor(
+    private patientService: PatientService,
+    private bookingService: BookingService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAllPatients();
+    this.createSearchForm();
+    this.getSearchFormValues();
+
+    this.authService.getUser().subscribe((user) => (this.currentUser = user));
+  }
+
+  loadAllPatients() {
+    this.patients$ = this.patientService.getAllPatients();
+  }
+
+  createSearchForm() {
+    this.searchForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+    });
+  }
+
+  getSearchFormValues() {
+    this.searchForm?.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe((value) => {
+        if (value.firstName === '' && value.lastName === '') {
+          this.loadAllPatients();
+        } else {
+          console.log(value.firstName, value.lastName);
+          this.patients$ = this.patientService.searchForPatient(
+            value.firstName,
+            value.lastName
+          );
+        }
+      });
+  }
+
+  showInformation(patient: Patient) {
+    this.selectedPatient = patient;
+    this.showAdditionalInformation = true;
+  }
+
+  createAppointment(selectedPatient: Patient) {
+    if (!this.pickedTime) return;
+    let startDate = new Date(this.pickedTime);
+    let endDate = new Date(startDate.getTime() + 30 * 60000);
+    const appointment = {
+      attendees: [
+        {
+          attendeeType: AttendeeType.PROVIDER,
+          entity: {
+            entityNo: this.currentUser?.['entityNo'],
+            firstName: this.currentUser?.['firstName'],
+            lastName: this.currentUser?.['lastName'],
+          },
+          entityNo: Roles.Doctor,
+        },
+        {
+          attendeeType: AttendeeType.PATIENT,
+          entity: {
+            entityNo: selectedPatient.entityNo,
+            firstName: selectedPatient.firstName,
+            lastName: selectedPatient.lastName,
+          },
+          entityNo: selectedPatient.entityNo,
+        },
+      ],
+      startDate: startDate.toISOString(),
+      description: 'appointment for patient',
+      title: 'appointment',
+      endDate: endDate.toISOString(),
+      id: 0,
+      organiser: Roles.Doctor,
+    };
+    this.bookingService.createBooking(appointment).subscribe((x) => {
+      const bookingUpdateBody: BookingStatusUpdateRequest = {
+        bookingStatus: Status.CONFIRMED,
+        comment: '',
+        includeDependent: true,
+      };
+      this.bookingService
+        .updateBooking(x.id, bookingUpdateBody)
+        .subscribe(() => {
+          this.showAdditionalInformation = false;
+        });
+    });
+  }
 }
