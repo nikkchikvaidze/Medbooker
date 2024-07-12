7c1be3fb2fd18f604e02210b29fa986a9f6c8877
Nikolozi <nikkchikvaidze@gmail.com>
2024-07-12 15:34:43 +0400

    added patients effect for store

--- src/app/models/booking.model.ts
+++ src/app/models/booking.model.ts
@@ -43,7 +43,7 @@ export interface StatusChange {
 export interface Attendee {
   attendeeType: AttendeeType;
   entity: Patient;
-  entityNo: number;
+  entityNo: number | undefined;
 }
 
 export enum AttendeeType {
